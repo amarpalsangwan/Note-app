0b7686500aefad39cd8d75c57b76a8aa4f0a2666
Amarpal <amits13420023@gmail.com>
2024-05-19 17:45:34 +0530

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname,"/views"));
 
 app.get("/", function (req, res) {
   fs.readdir(`./files`, function (err, files) {
